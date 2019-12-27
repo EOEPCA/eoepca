cb107417b72b71e532e2fac0da1697f71aa06b1c
josejulioramos <josejulioramos@outlook.com>
2019-12-27 10:17:16 +0000

    Update secrets.tf

--- test/template-svce/secrets.tf
+++ test/template-svce/secrets.tf
@@ -6,7 +6,9 @@ resource "kubernetes_secret" "db-user-pass" {
 
   data = {
     username = var.db_username
+    db_username = var.db_username
     password = var.db_password
+    db_password = var.db_password
   }
 
   type = "kubernetes.io/basic-auth"
