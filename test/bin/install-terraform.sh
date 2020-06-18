a7ced7f2c9c44143d2c735fbeb6f63bf249fcbb1
Richard Conway <richard@rconway.co.uk>
2020-06-18 12:58:59 +0100

    EOEPCA-105 Install 7z

--- test/bin/install-terraform.sh
+++ test/bin/install-terraform.sh
@@ -9,6 +9,7 @@ trap "cd '${ORIG_DIR}'" EXIT
 mkdir -p $HOME/.local/bin
 
 # terraform
+sudo apt-get -y install p7zip-full
 curl -sLo terraform.zip https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 7z x terraform.zip
 rm -f terraform.zip
