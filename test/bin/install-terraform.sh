cbabcb7a582d5f51ba2bc4f3aa951ed58e9bf9a1
Richard Conway <richard@rconway.co.uk>
2020-06-18 12:36:10 +0100

    EOEPCA-105 Attempt alternative minikube approach

---
+++ test/bin/install-terraform.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+trap "cd '${ORIG_DIR}'" EXIT
+
+mkdir -p $HOME/.local/bin
+
+# terraform
+curl -sLo terraform.zip https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
+7z x terraform.zip
+rm -f terraform.zip
+chmod +x terraform
+mv terraform $HOME/.local/bin
--- test/bin/setup-minikube.sh
+++ test/bin/setup-minikube.sh
@@ -23,3 +23,7 @@ curl -Lo $HOME/.local/bin/minikube https://storage.googleapis.com/minikube/relea
 # start minikube
 # - default container runtime is docker - see https://minikube.sigs.k8s.io/docs/handbook/config/#runtime-configuration
 minikube start --mount --mount-string="$KUBE_DATA:/mnt/eoepca"
+
+# Wait for Kubernetes to be up and ready.
+echo "##### Waiting for kubernetes cluster to be ready"
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
