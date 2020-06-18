b2eff6e061d2347b9e610391c21ddc0348b69ef7
Richard Conway <richard@rconway.co.uk>
2020-06-18 13:07:20 +0100

    EOEPCA-105 Fix terraform install

--- test/bin/install-terraform.sh
+++ test/bin/install-terraform.sh
@@ -9,9 +9,12 @@ trap "cd '${ORIG_DIR}'" EXIT
 mkdir -p $HOME/.local/bin
 
 # terraform
-sudo apt-get -y install p7zip-full
+if ! unzip --help >/dev/null 2>&1
+then
+  sudo apt-get -y install unzip
+fi
 curl -sLo terraform.zip https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
-7z x terraform.zip
+unzip terraform.zip
 rm -f terraform.zip
 chmod +x terraform
 mv terraform $HOME/.local/bin
