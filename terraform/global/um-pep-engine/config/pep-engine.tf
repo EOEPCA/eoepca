92d750f6394568d6c84257f662650c424a16f2b5
rconway <richard@rconway.co.uk>
2020-06-22 11:03:34 +0000

    Edits for my local env

--- terraform/global/um-pep-engine/config/pep-engine.tf
+++ terraform/global/um-pep-engine/config/pep-engine.tf
@@ -5,7 +5,7 @@ resource "kubernetes_config_map" "pep_engine_cm" {
 
   data = {
     PEP_REALM                    = "eoepca"
-    PEP_AUTH_SERVER_URL          = "https://eoepca-dev.deimos-space.com"
+    PEP_AUTH_SERVER_URL          = "https://test.eoepca.org"
     PEP_PROXY_ENDPOINT           = "/"
     PEP_SERVICE_HOST             = "0.0.0.0"
     PEP_SERVICE_PORT             = "5566"
@@ -13,7 +13,7 @@ resource "kubernetes_config_map" "pep_engine_cm" {
     PEP_CHECK_SSL_CERTS          = "false"
     PEP_USE_THREADS              = "true"
     PEP_DEBUG_MODE               = "true"
-    PEP_RESOURCE_SERVER_ENDPOINT = "http://eoepca-ades-core/"
+    PEP_RESOURCE_SERVER_ENDPOINT = "http://ades/"
   }
 }
 
