e923b9a15e6e94310adf6bf92b7c9a8268225816
Richard Conway <richard@rconway.co.uk>
2020-07-21 14:02:40 +0100

    [staging] Added script to generate RKE cluster.yml file from terraform outputs (Creodias)

--- kubernetes/create-cluster-config.sh
+++ kubernetes/create-cluster-config.sh
@@ -7,16 +7,18 @@ cd "${ORIG_DIR}"
 
 trap "cd '${ORIG_DIR}'" EXIT
 
+CLUSTER_YML_FILE="${1:-cluster.yml}"
+
 function master_nodes() {
   master_nodes=$(terraform output -state=../creodias/terraform.tfstate -json | jq -r '.k8s_master_ips.value[]')
   for node in $master_nodes
   do
     cat - <<EOF
-    - address: $node
-      user: eouser
-      role:
-        - controlplane
-        - etcd
+  - address: $node
+    user: eouser
+    role:
+      - controlplane
+      - etcd
 EOF
   done
 }
@@ -26,20 +28,42 @@ function worker_nodes() {
   for node in $worker_nodes
   do
     cat - <<EOF
-    - address: $node
-      user: eouser
-      role:
-        - worker
+  - address: $node
+    user: eouser
+    role:
+      - worker
 EOF
   done
 }
 
+function bastion_host() {
+  bastion=$(terraform output -state=../creodias/terraform.tfstate -json | jq -r '.bastion_fips.value[]')
+  cat - <<EOF
+bastion_host:
+  address: $bastion
+  user: eouser
+EOF
+}
+
+function cluster_yml() {
+  cat - <<EOF
+nodes:
+$(master_nodes)
+$(worker_nodes)
+
+ingress:
+  provider: none
+
+addons_include:
+  - ./ingress-controller/ingress-nginx-nodeport.yml
+
+$(bastion_host)
+EOF
+}
+
 function main() {
-  cp cluster-template.yml cluster.yml
-  m="$(master_nodes)"
-  w="$(worker_nodes)"
-  sed -i "s/MASTER_NODES/$m/" cluster.yml
-  sed -i "s/WORKER_NODES/$w/" cluster.yml
+  cluster_yml | tee "${CLUSTER_YML_FILE}"
+  echo -e "---\nOutput written to file: ${CLUSTER_YML_FILE}"
 }
 
 main
