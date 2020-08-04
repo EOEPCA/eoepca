ecff8d08575c55f82012ea087526733436cd241d
Richard Conway <richard@rconway.co.uk>
2020-08-04 15:08:42 +0000

    Updated minikube setup to account for running natively in a VM

--- minikube/setup-minikube.sh
+++ minikube/setup-minikube.sh
@@ -13,9 +13,26 @@ echo "Download minikube..."
 curl -sLo $HOME/.local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x $HOME/.local/bin/minikube
 
-# start minikube
-# - default container runtime is docker - see https://minikube.sigs.k8s.io/docs/handbook/config/#runtime-configuration
-echo "Start minikube, and wait for cluster..."
-minikube start --addons ingress --wait "all"
+# minikube (native)
+if [ "$1" = "native" ]
+then
+  if hash conntrack
+  then
+    # start minikube
+    # - default container runtime is docker - see https://minikube.sigs.k8s.io/docs/handbook/config/#runtime-configuration
+    echo "Start minikube (native), and wait for cluster..."
+    sudo -E $HOME/.local/bin/minikube start --driver=none --addons ingress --wait "all"
+    sudo chown -R $USER $HOME/.kube $HOME/.minikube
+  else
+    echo "ERROR: conntrack must be installed for minikube driver='none', e.g. 'sudo apt install conntrack'. Aborting..."
+    exit 1
+  fi
+# minikube docker
+else
+  # start minikube
+  # - default container runtime is docker - see https://minikube.sigs.k8s.io/docs/handbook/config/#runtime-configuration
+  echo "Start minikube (default), and wait for cluster..."
+  minikube start --addons ingress --wait "all"
+fi
 
 echo "...READY"
