7fa23a42397b3fdd9a275a2b0647d6088a504b01
Richard Conway <richard@rconway.co.uk>
2020-10-23 15:38:15 +0100

    EOEPCA-167 ades.conf ConfigMap use determined value for workspace URL

--- terraform/global/ades02/ades.sh
+++ terraform/global/ades02/ades.sh
@@ -33,8 +33,40 @@ function prepareKubeconfig() {
     kubectl -n "${NAMESPACE}" create configmap ades-kubeconfig --from-file=${KUBECONFIG_FILE} --dry-run=client -o yaml >>${K8S_YAML_FILE}
 }
 
+function prepareAdesConf() {
+    storage_host="http://$(kubectl get ingress/workspace -o json 2>/dev/null | jq -r '.spec.rules[0].host')/"
+    cat - <<EOF >ades.conf
+# this is already set to the volume configured in the docker file
+KUBECONFIG=/var/etc/ades/kubeconfig
+# This is the resource manager endpoint of the catalog used for processing results publication
+# CATALOG_ENDPOINT=https://catalog.terradue.com/rconway
+CATALOG_ENDPOINT=aHR0cHM6Ly9jYXRhbG9nLnRlcnJhZHVlLmNvbS9yY29ud2F5
+# catalog username
+# CATALOG_USERNAME=rconway
+CATALOG_USERNAME=cmNvbndheQ==
+# catalog password (may be base64 encoded)
+# CATALOG_APIKEY=AKCp8hyEYSMqk4swUndyCDVZW7g3XFYfvE2xVFQEdYagJLqVJY1qKbXcmStLoUFEyEhL5tKcY
+CATALOG_APIKEY=QUtDcDhoeUVZU01xazRzd1VuZHlDRFZaVzdnM1hGWWZ2RTJ4VkZRRWRZYWdKTHFWSlkxcUtiWGNtU3RMb1VGRXlFaEw1dEtjWQ==
+# This is the resource manager endpoint of the store used for processing results persistent storage (WebDAV)
+# Constructed using the value of 'ingress/workspace'...
+# STORAGE_HOST=${storage_host}  [constructed]
+STORAGE_HOST=$(echo -n "${storage_host}" | base64)
+# store username
+# STORAGE_USERNAME=eoepca
+STORAGE_USERNAME=ZW9lcGNh
+# store password (may be base64 encoded)
+# STORAGE_APIKEY=telespazio
+STORAGE_APIKEY=dGVsZXNwYXppbw==
+# kubernetes storage class to be used for provisioning volumes. Must be a persistent volume claim compliant (glusterfs-storage)
+STORAGE_CLASS=standard
+# Size of the Kubernetes Volumes in gigabytes
+VOLUME_SIZE=4
+EOF
+}
+
 function prepareADES() {
     # ades-config ConfigMap
+    prepareAdesConf
     echo "---"  >>${K8S_YAML_FILE}
     kubectl -n "${NAMESPACE}" create configmap ades-config --from-env-file=ades.conf --dry-run=client -o yaml >>${K8S_YAML_FILE}
 
