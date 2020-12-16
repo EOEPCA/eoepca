d3bd95c5490aef728da5510a693d7d7a4b714b1b
Tiago Fernandes <tiago.fernandes@elecnor.es>
2020-12-16 16:18:36 +0000

    image version updates

--- terraform/global/um-pep-engine/pep-engine.tf
+++ terraform/global/um-pep-engine/pep-engine.tf
@@ -128,7 +128,7 @@ resource "kubernetes_deployment" "pep-engine" {
         }
         container {
           name  = "pep-engine"
-          image = "eoepca/um-pep-engine:task203_2"
+          image = "eoepca/um-pep-engine:v0.3"
 
           port {
             container_port = 5566
