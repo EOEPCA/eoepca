38b3626129167a6787157da10ffadc3bc117d4f1
Richard Conway <richard@rconway.co.uk>
2021-01-30 11:24:38 +0000

    Testing how dynamic PVCs work when the provisioner is deleted

--- terraform/global/nfs-provisioner/03-class.tf
+++ terraform/global/nfs-provisioner/03-class.tf
@@ -5,3 +5,11 @@ resource "kubernetes_storage_class" "managed_nfs_storage" {
   storage_provisioner = "nfs-storage"
   reclaim_policy = "Retain"
 }
+
+# resource "kubernetes_storage_class" "ractest_nfs_storage" {
+#   metadata {
+#     name = "ractest-nfs-storage"
+#   }
+#   storage_provisioner = "ractest-storage"
+#   reclaim_policy = "Retain"
+# }
