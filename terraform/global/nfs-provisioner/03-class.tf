64e0c24d2abd77c11c43d41d765b562870a14e27
Richard Conway <richard@rconway.co.uk>
2021-02-09 09:00:48 +0000

    EOEPCA-291 Set ReclaimPolicy to Delete for nfs-provisioner

--- terraform/global/nfs-provisioner/03-class.tf
+++ terraform/global/nfs-provisioner/03-class.tf
@@ -3,7 +3,7 @@ resource "kubernetes_storage_class" "managed_nfs_storage" {
     name = "managed-nfs-storage"
   }
   storage_provisioner = "nfs-storage"
-  reclaim_policy = "Retain"
+  reclaim_policy = "Delete"
 }
 
 # resource "kubernetes_storage_class" "ractest_nfs_storage" {
