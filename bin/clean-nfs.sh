333892312d9b0e3a90c7a75074c18657eabbfe61
Richard Conway <richard@rconway.co.uk>
2021-02-15 21:03:35 +0000

    Corrected clean-nfs script

--- bin/clean-nfs.sh
+++ bin/clean-nfs.sh
@@ -8,5 +8,5 @@ trap "cd '${ORIG_DIR}'" EXIT
 
 for i in $(kubectl get pv -A | grep Released | cut -d' ' -f 1)
 do
-  ssh eouser@192.168.123.10 ls -ld /data/dynamic/$i
+  ssh eouser@192.168.123.10 ls -ld /data/dynamic/\*$i\*
 done
