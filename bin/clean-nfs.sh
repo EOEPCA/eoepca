62a76741662e2b2393e696cafc16dced76deabe1
Richard Conway <richard@rconway.co.uk>
2021-02-15 20:20:09 +0000

    Add script to clean the CREODIAS nfs (dynamic)

---
+++ bin/clean-nfs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+trap "cd '${ORIG_DIR}'" EXIT
+
+for i in $(kubectl get pv -A | grep Released | cut -d' ' -f 1)
+do
+  ssh eouser@192.168.123.10 ls -ld /data/dynamic/$i
+done
