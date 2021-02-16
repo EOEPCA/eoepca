85b7c35432a5dd6f68e509d38f2cc9d119d80018
Richard Conway <richard@rconway.co.uk>
2021-02-16 16:44:45 +0000

    Tweaks to clean-nfs script

--- bin/clean-nfs.sh
+++ bin/clean-nfs.sh
@@ -8,5 +8,7 @@ trap "cd '${ORIG_DIR}'" EXIT
 
 for i in $(kubectl get pv -A | grep Released | cut -d' ' -f 1)
 do
-  ssh eouser@192.168.123.10 ls -ld /data/dynamic/\*$i\*
+  # ssh eouser@192.168.123.10 ls -ld /data/dynamic/\*$i\*
+  # ssh eouser@192.168.123.10 sudo rm -rf /data/dynamic/\*$i\*
+  kubectl delete pv/$i
 done
