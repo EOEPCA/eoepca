e9acf444b9b9f63338eafa34c926b9b4f238de14
Richard Conway <richard@rconway.co.uk>
2021-03-29 11:47:22 +0100

    EOEPCA-289 [demo] add install script for kubeseal CLI

---
+++ bin/install-kubeseal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+trap "cd '${ORIG_DIR}'" EXIT
+
+mkdir -p $HOME/.local/bin
+
+# kubeseal
+# curl -sLo $HOME/.local/bin/kubeseal https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.15.0/kubeseal-linux-amd64
+curl -sLo $HOME/.local/bin/kubeseal https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.10.0/kubeseal-linux-amd64
+chmod +x $HOME/.local/bin/kubeseal
