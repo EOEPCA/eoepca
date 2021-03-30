c9108d8055132ee4771740d3f0cb3ff1e83dd930
Richard Conway <richard@rconway.co.uk>
2021-03-30 13:45:36 +0100

    EOEPCA289 use infra ns for sealed secret controller

--- system/clusters/bootstrap/bootstrap.sh
+++ system/clusters/bootstrap/bootstrap.sh
@@ -19,7 +19,7 @@ kubectl create namespace um
 # Sealed Secrets
 if test -f "sealed-secrets-master.key"; then kubectl apply -f "sealed-secrets-master.key"; fi
 helm repo add bitnami-sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm upgrade -i --version 1.13.2 --create-namespace --namespace flux-system eoepca-sealed-secrets bitnami-sealed-secrets/sealed-secrets
+helm upgrade -i --version 1.13.2 --create-namespace --namespace infra eoepca-sealed-secrets bitnami-sealed-secrets/sealed-secrets
 
 # Cert Manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.crds.yaml
