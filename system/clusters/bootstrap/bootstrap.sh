2510128b6fdc218b08891621a21352bbf3f792db
Richard Conway <richard@rconway.co.uk>
2021-03-30 12:36:14 +0100

    EOEPCA-289 [demo] add initial creation of namespaces

--- system/clusters/bootstrap/bootstrap.sh
+++ system/clusters/bootstrap/bootstrap.sh
@@ -6,6 +6,16 @@ BIN_DIR="$(pwd)"
 
 trap "cd '${ORIG_DIR}'" EXIT
 
+# Namespaces
+kubectl create namespace common
+kubectl create namespace demo
+kubectl create namespace infra
+kubectl create namespace proc
+kubectl create namespace rm
+kubectl create namespace rm-user-1
+kubectl create namespace test
+kubectl create namespace um
+
 # Sealed Secrets
 if test -f "sealed-secrets-master.key"; then kubectl apply -f "sealed-secrets-master.key"; fi
 helm repo add bitnami-sealed-secrets https://bitnami-labs.github.io/sealed-secrets
