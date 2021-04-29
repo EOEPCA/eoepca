dd0d545b00c40a03f9d2513a4e74c556f5b222a6
Richard Conway <richard@rconway.co.uk>
2021-04-29 09:03:01 +0100

    EOEPCA-289 use 'domain' variable in test client main to easily switch between target deployments

--- test/client/main.py
+++ test/client/main.py
@@ -5,11 +5,12 @@ from time import sleep
 
 def main():
     print("\n### TEST CLIENT ###")
-    USER_NAME="UserA"
+    USER_NAME="eric"
     USER_PASSWORD="defaultPWD"
-    base_url = "https://test.185.52.193.87.nip.io"
-    ades_resource_api_url = "http://ades.resources.185.52.193.87.nip.io"
-    ades_url = "http://ades.test.185.52.193.87.nip.io"
+    domain = "185.52.193.87.nip.io"
+    base_url = "https://test." + domain
+    ades_resource_api_url = "http://ades.resources." + domain
+    ades_url = "http://ades.test." + domain
     ades_user = USER_NAME
     ades_user_prefix = "/" + ades_user
 
@@ -47,7 +48,7 @@ def main():
     #===========================================================================
 
     # workarounds
-    # ades_url_direct = "http://ades.185.52.193.87.nip.io/testuser"
+    # ades_url_direct = "http://ades." + domain + "/testuser"
     # ades_jwt_example = "eyJhbGciOiJIUzI1NiIsImtpZCI6IlJTQTEifQ.eyJhY3RpdmUiOnRydWUsImV4cCI6MTU5MzUxNTU2NSwiaWF0IjoxNTkzNTExOTY1LCJuYmYiOm51bGwsInBlcm1pc3Npb25zIjpbeyJyZXNvdXJjZV9pZCI6ImI3Y2FkZTVjLTM3MmYtNGM4Ny1iZTgyLWE3OTU2NDk4ZTcyOSIsInJlc291cmNlX3Njb3BlcyI6WyJBdXRoZW50aWNhdGVkIiwib3BlbmlkIl0sImV4cCI6MTU5MzUxNTU2NCwicGFyYW1zIjpudWxsfV0sImNsaWVudF9pZCI6IjYxY2UyOGQ1LWFhMTYtNGRkYy04NDJmLWZjYzE1OGQzMTVmYSIsInN1YiI6bnVsbCwiYXVkIjoiNjFjZTI4ZDUtYWExNi00ZGRjLTg0MmYtZmNjMTU4ZDMxNWZhIiwiaXNzIjpudWxsLCJqdGkiOm51bGwsInBjdF9jbGFpbXMiOnsiYXVkIjpbIjYxY2UyOGQ1LWFhMTYtNGRkYy04NDJmLWZjYzE1OGQzMTVmYSJdLCJzdWIiOlsiZWIzMTQyMWUtMGEyZS00OTBmLWJiYWYtMDk3MWE0ZTliNzhhIl0sInVzZXJfbmFtZSI6WyJyaWNvbndheSJdLCJpc3MiOlsiaHR0cHM6Ly9lb2VwY2EtZGV2LmRlaW1vcy1zcGFjZS5jb20iXSwiZXhwIjpbIjE1OTM1MTU1NjQiXSwiaWF0IjpbIjE1OTM1MTE5NjQiXSwib3hPcGVuSURDb25uZWN0VmVyc2lvbiI6WyJvcGVuaWRjb25uZWN0LTEuMCJdfX0.d5qeaqLfl0oh9KigVrM_lT1hZMaOzQBFB7jjaKI3PjE"
     # jwt_from_pep = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlJTQTEifQ.eyJhY3RpdmUiOnRydWUsImV4cCI6MTYwNDM0MjIzNCwiaWF0IjoxNjA0MzM4NjM0LCJuYmYiOm51bGwsInBlcm1pc3Npb25zIjpbeyJyZXNvdXJjZV9pZCI6IjZjNThhNWU1LTk1YjctNDRjYS1hYjQ5LWIxMTkyZTBkYjE5OCIsInJlc291cmNlX3Njb3BlcyI6WyJBdXRoZW50aWNhdGVkIiwib3BlbmlkIl0sImV4cCI6MTYwNDM0MjIzNCwicGFyYW1zIjpudWxsfV0sImNsaWVudF9pZCI6Ijk2M2U0YzJhLTk5MjQtNGM0ZC1iOTk5LWJlYmU3OWM5NmE1ZSIsInN1YiI6bnVsbCwiYXVkIjoiOTYzZTRjMmEtOTkyNC00YzRkLWI5OTktYmViZTc5Yzk2YTVlIiwiaXNzIjpudWxsLCJqdGkiOm51bGwsInBjdF9jbGFpbXMiOnsiYXVkIjpbIjk2M2U0YzJhLTk5MjQtNGM0ZC1iOTk5LWJlYmU3OWM5NmE1ZSJdLCJzdWIiOlsiNjk0N2RlYWEtNTZjYi00YjM2LTk2ODktYjU4MzA0YjAzNWZlIl0sImlzcyI6WyJodHRwczovL3Rlc3QuMTkyLjE2OC40OS4yLm5pcC5pbyJdLCJleHAiOlsiMTYwNDM0MjIzNCJdLCJpYXQiOlsiMTYwNDMzODYzNCJdLCJveE9wZW5JRENvbm5lY3RWZXJzaW9uIjpbIm9wZW5pZGNvbm5lY3QtMS4wIl19fQ.RcQmVNrt8C4SgxS7eZTbG-Yw_xnFaOY8yNRvZ4n6DqVaAU8daVXl3EwTiVsNNgB_Vr3nG5zrc7kKJXWMgVM-jF-L2B-smMNRzCpwcF0UwJMmZ3iaCpmtuh_AZtB9SqJurLNIYprEnLh1YcMa2RWjxq6pMjGdMKRCE7Z85c3hFB8_TpsAB004VQd_vXxIP7efpE5YhzQwK2N5cYhUsru2PmBuLpPDvSA6wUWxqQyCEF8IWqeR1DBvhmzV_seTFKhT1GdkVUIsB5RnP5A4OBwBkY3oC-b0euI68MtjMas5i_i0f-jmQjc2816DTLhniySAUJBTwDOyx7VlhrEbBmqM_g"
     # active_ades_url = ades_url_direct
