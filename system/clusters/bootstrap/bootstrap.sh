86aa16490aa586f43358453180f56048f885a7dd
Richard Conway <richard@rconway.co.uk>
2021-05-14 13:45:54 +0100

    Corrected namespace and name of cert-manager helm release

--- system/clusters/bootstrap/bootstrap.sh
+++ system/clusters/bootstrap/bootstrap.sh
@@ -24,4 +24,4 @@ helm upgrade -i --version 1.13.2 --create-namespace --namespace infra eoepca-sea
 # Cert Manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io
-helm upgrade -i --version v1.2.0 --create-namespace --namespace infra eoepca-cert-manager jetstack/cert-manager
+helm upgrade -i --version v1.2.0 --create-namespace --namespace cert-manager cert-manager jetstack/cert-manager
