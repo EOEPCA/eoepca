1203580e36de648cb9eeaaa0262ccc139f9428ed
Ricardo Noguera <ricardo.noguera@telespazio.com>
2021-06-09 11:36:18 +0100

    Delete charts to avoid confusion

--- system/charts/um-login-service-helm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ORIG_DIR="$(pwd)"
-cd "$(dirname "$0")"
-BIN_DIR="$(pwd)"
-
-trap "cd '${ORIG_DIR}'" EXIT
-
-helm upgrade -i -f um-login-service-values.yaml rac-um-login-service login-service
--- system/charts/um-pdp-engine-helm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ORIG_DIR="$(pwd)"
-cd "$(dirname "$0")"
-BIN_DIR="$(pwd)"
-
-trap "cd '${ORIG_DIR}'" EXIT
-
-helm upgrade -i -f um-pdp-engine-values.yaml rac-um-pdp-engine pdp-engine
