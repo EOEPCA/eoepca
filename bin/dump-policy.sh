c01a272a5e3ab090efa2e8947d322611b96b6db3
Richard Conway <richard@rconway.co.uk>
2021-07-07 11:28:35 +0100

    Add workspace-api to UM policy helper scripts

--- bin/dump-policy.sh
+++ bin/dump-policy.sh
@@ -7,8 +7,13 @@ BIN_DIR="$(pwd)"
 trap "cd '${ORIG_DIR}'" EXIT
 
 # ades-pep
-echo -n "Dumping policy for ades-pep to $ORIG_DIR/pep.json..."
-kubectl -n proc exec -it svc/proc-ades-pep -c proc-ades-pep -- management_tools list --all | jq > "$ORIG_DIR"/pep.json
+echo -n "Dumping policy for ades-pep to $ORIG_DIR/ades-pep.json..."
+kubectl -n proc exec -it svc/proc-ades-pep -c proc-ades-pep -- management_tools list --all | jq > "$ORIG_DIR"/ades-pep.json
+echo " done"
+
+# workspace-api-pep
+echo -n "Dumping policy for workspace-api-pep to $ORIG_DIR/workspace-api-pep.json..."
+kubectl -n rm exec -it svc/workspace-api-pep -c workspace-api-pep -- management_tools list --all | jq > "$ORIG_DIR"/workspace-api-pep.json
 echo " done"
 
 # pdp
