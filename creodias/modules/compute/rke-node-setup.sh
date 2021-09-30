c912a12392844f2dadfbec7ec287d5503452c528
Richard Conway <richard@rconway.co.uk>
2021-09-30 17:20:39 +0100

    Add new node to cluster

--- creodias/modules/compute/rke-node-setup.sh
+++ creodias/modules/compute/rke-node-setup.sh
@@ -18,6 +18,16 @@ echo "Setting up node for USER=${USER}"
 if ! hash docker 2>/dev/null
 then
   export VERSION=19.03
-  curl -sfL https://get.docker.com | sh -s -
+  curl -sfLo docker.sh https://get.docker.com
+  chmod +x docker.sh
+  let status=1
+  while test $status -ne 0
+  do
+    ./docker.sh
+    let status=$?
+    echo "Docker install status: ${status}"
+    sleep 5
+  done
+  rm docker.sh
   sudo usermod -aG docker ${USER}
 fi
