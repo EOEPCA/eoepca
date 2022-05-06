86e6c9c9b70a7e08b0404461626c8f766abc1446
Richard Conway <richard@rconway.co.uk>
2022-05-06 16:00:57 +0100

    Upgrade cert-manager to v1.8.0

--- system/clusters/bootstrap/bootstrap.sh
+++ system/clusters/bootstrap/bootstrap.sh
@@ -21,6 +21,7 @@ helm repo add bitnami-sealed-secrets https://bitnami-labs.github.io/sealed-secre
 helm upgrade -i --version 1.13.2 --create-namespace --namespace infra eoepca-sealed-secrets bitnami-sealed-secrets/sealed-secrets
 
 # Cert Manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.crds.yaml
+# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io
-helm upgrade -i --version v1.2.0 --create-namespace --namespace cert-manager cert-manager jetstack/cert-manager
+helm upgrade -i --version v1.8.0 --create-namespace --namespace cert-manager --set installCRDs=true cert-manager jetstack/cert-manager
+# helm upgrade -i --version v1.8.0 --create-namespace --namespace cert-manager cert-manager jetstack/cert-manager
