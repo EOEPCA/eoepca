a3475973219413e0c7a51550829d37518385b821
Richard Conway <richard@rconway.co.uk>
2022-05-24 11:09:21 +0100

    Update the 'list all container images' script

--- bin/list-container-images.sh
+++ bin/list-container-images.sh
@@ -9,6 +9,17 @@ onExit() {
 }
 trap onExit EXIT
 
+# ref. https://kubernetes.io/docs/tasks/access-application-cluster/list-all-running-container-images/
+
+# List all container images
+#
+# kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" |\
+#   tr -s '[[:space:]]' '\n' |\
+#   sort |\
+#   uniq -c
+
+# List container images by pod
+#
 kubectl get pods --all-namespaces \
-  -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}' \
-  | sort
+  -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{":\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}' |\
+  sort
