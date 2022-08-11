160b40cb15c8bba94a1a6df87559fd5632b67deb
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2022-08-11 10:19:23 +0000

    Updated passportLogin customization

--- system/clusters/creodias/user-management/deploy_customizations.sh
+++ system/clusters/creodias/user-management/deploy_customizations.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+NAMESPACE="staging"
 cd customizations/oxauth
 tar -czvf ../../customizations_oxauth.tar.gz .
 cd ../oxtrust
 tar -czvf ../../customizations_oxtrust.tar.gz .
 cd ../../
-kubectl create configmap front-customizations --from-file=customizations_oxauth.tar.gz --from-file=customizations_oxtrust.tar.gz -o yaml --dry-run | kubectl replace -f -
-OXAUTH=$(kubectl get deployment | grep oxauth | awk '{print $1}')
-OXTRUST=$(kubectl get statefulsets | grep oxtrust | awk '{print $1}')
-kubectl scale deployment $OXAUTH --replicas 0
-kubectl scale statefulsets $OXTRUST --replicas 0
+kubectl create configmap front-customizations -n $NAMESPACE --from-file=customizations_oxauth.tar.gz --from-file=customizations_oxtrust.tar.gz -o yaml --dry-run | kubectl replace -f -
+OXAUTH=$(kubectl get deployment -n $NAMESPACE | grep oxauth | awk '{print $1}')
+OXTRUST=$(kubectl get statefulsets -n $NAMESPACE | grep oxtrust | awk '{print $1}')
+kubectl scale deployment $OXAUTH --replicas 0 -n $NAMESPACE
+kubectl scale statefulsets $OXTRUST --replicas 0 -n $NAMESPACE
 sleep 5
-kubectl scale deployment $OXAUTH --replicas 1
-kubectl scale statefulsets $OXTRUST --replicas 1
+kubectl scale deployment $OXAUTH --replicas 1 -n $NAMESPACE
+kubectl scale statefulsets $OXTRUST --replicas 1 -n $NAMESPACE
