1e7a8691fad426d9d7606a941e74e810694e646b
Ricardo Noguera <ricardo.noguera@telespazio.com>
2022-08-18 14:22:57 +0100

    change secrets

---
+++ system/clusters/creodias/resource-management/bucket-operator/ss-openstack-access-create.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+onExit() {
+  cd "${ORIG_DIR}"
+}
+
+trap onExit EXIT
+
+SECRET_NAME="openstack"
+NAMESPACE="rm"
+
+DOMAIN="${1:-set-domain-here}"
+USER="${2:-set-user-here}"
+PASS="${3:-set-pass-here}"
+
+clientConfigFile() {
+  cat - <<EOF
+domainname: ${DOMAIN}
+password: ${PASS}
+username: ${USER="${2:-set-user-here}"
+}
+EOF
+}
+
+secretYaml() {
+  kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
+    --from-literal="client.yaml=$(clientConfigFile)" \
+    --dry-run=client -o yaml
+}
+
+# Create Secret and then pipe to kubeseal to create the SealedSecret
+secretYaml | kubeseal -o yaml --controller-name eoepca-sealed-secrets --controller-namespace infra > ${SECRET_NAME}-access.yaml
