c0b3b47e0977f19a2f8137f41c2b1efb13dbe07a
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2022-09-19 11:14:29 +0000

    cleanup in customization script

--- system/clusters/creodias/user-management/deploy_customizations.sh
+++ system/clusters/creodias/user-management/deploy_customizations.sh
@@ -6,6 +6,7 @@ cd ../oxtrust
 tar -czvf ../../customizations_oxtrust.tar.gz .
 cd ../../
 kubectl create configmap front-customizations -n $NAMESPACE --from-file=customizations_oxauth.tar.gz --from-file=customizations_oxtrust.tar.gz -o yaml --dry-run | kubectl replace -f -
+rm -rf ./*.tar.gz
 OXAUTH=$(kubectl get deployment -n $NAMESPACE | grep oxauth | awk '{print $1}')
 OXTRUST=$(kubectl get statefulsets -n $NAMESPACE | grep oxtrust | awk '{print $1}')
 kubectl scale deployment $OXAUTH --replicas 0 -n $NAMESPACE
