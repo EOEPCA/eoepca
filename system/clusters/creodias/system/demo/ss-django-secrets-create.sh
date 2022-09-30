f64afb1b6e93371f0fe9554a07a63afcf789f7f0
Richard Conway <richard@rconway.co.uk>
2022-09-30 15:33:57 +0100

    Update sealed secret for portal

---
+++ system/clusters/creodias/system/demo/ss-django-secrets-create.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+onExit() {
+  cd "${ORIG_DIR}"
+}
+
+trap onExit EXIT
+
+SECRET_NAME="django-secrets"
+NAMESPACE="demo"
+
+client_id="${1:-set-client-id-here}"
+client_secret="${2:-set-client-secret-here}"
+django_secret="${3:-set-django-secret-here}"
+
+secretYaml() {
+  kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
+    --from-literal=OIDC_RP_CLIENT_ID="${client_id}" \
+    --from-literal=OIDC_RP_CLIENT_SECRET="${client_secret}" \
+    --from-literal=DJANGO_SECRET="${django_secret}" \
+    --dry-run=client -o yaml
+}
+
+# Create Secret and then pipe to kubeseal to create the SealedSecret
+secretYaml | kubeseal -o yaml --controller-name eoepca-sealed-secrets --controller-namespace infra > ss-${SECRET_NAME}.yaml
