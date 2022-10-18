8a3002451abac7f789b3362e4320228957cb4bee
Richard Conway <richard@rconway.co.uk>
2022-10-18 16:45:24 +0100

    [UM] tidy up the deploy_customizations.sh script

--- system/clusters/creodias/user-management/deploy_customizations.sh
+++ system/clusters/creodias/user-management/deploy_customizations.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+onExit() {
+  cd "${ORIG_DIR}"
+}
+
 NAMESPACE="um"
+
+# Prepare tar files
 cd customizations/oxauth
 tar -czvf ../../customizations_oxauth.tar.gz .
 cd ../oxtrust
 tar -czvf ../../customizations_oxtrust.tar.gz .
 cd ../../
-kubectl create configmap front-customizations -n $NAMESPACE --from-file=customizations_oxauth.tar.gz --from-file=customizations_oxtrust.tar.gz -o yaml --dry-run | kubectl replace -f -
+
+# Create and apply ConfigMap from tar files
+kubectl create configmap front-customizations -n $NAMESPACE --from-file=customizations_oxauth.tar.gz --from-file=customizations_oxtrust.tar.gz -o yaml --dry-run=client | kubectl replace -f -
 rm -rf ./*.tar.gz
+
+# Restart oxauth and oxtrust to pick-up customizations
 OXAUTH=$(kubectl get deployment -n $NAMESPACE | grep oxauth | awk '{print $1}')
 OXTRUST=$(kubectl get statefulsets -n $NAMESPACE | grep oxtrust | awk '{print $1}')
 kubectl scale deployment $OXAUTH --replicas 0 -n $NAMESPACE
