e27f363239dc2bb5028bb73de6ad7386c35a12d9
Richard Conway <richard@rconway.co.uk>
2022-11-03 17:12:31 +0000

    Improve `dump-policy` script

--- bin/dump-policy.sh
+++ bin/dump-policy.sh
@@ -4,39 +4,39 @@ ORIG_DIR="$(pwd)"
 cd "$(dirname "$0")"
 BIN_DIR="$(pwd)"
 
-trap "cd '${ORIG_DIR}'" EXIT
+onExit() {
+  cd "${ORIG_DIR}"
+}
+trap onExit EXIT
 
-# combined-rm-pep
-echo -n "Dumping policy for combined-rm-pep to $ORIG_DIR/combined-rm-pep.json..."
-kubectl -n rm exec -it svc/combined-rm-pep -c combined-rm-pep -- management_tools list --all | jq > "$ORIG_DIR"/combined-rm-pep.json
-echo " done"
+main() {
+  if [ -z "$1" ]; then
+    dumpAll
+  else
+    dumpDeployment "$@"
+  fi
+}
 
-# # resource-catalogue-pep
-# echo -n "Dumping policy for resource-catalogue-pep to $ORIG_DIR/resource-catalogue-pep.json..."
-# kubectl -n rm exec -it svc/resource-catalogue-pep -c resource-catalogue-pep -- management_tools list --all | jq > "$ORIG_DIR"/resource-catalogue-pep.json
-# echo " done"
+dumpAll() {
+  dumpDeployment proc ades-pep
+  dumpDeployment rm combined-rm-pep
+  # dumpDeployment rm resource-catalogue-pep
+  # dumpDeployment rm data-access-pep
+  dumpDeployment rm workspace-api-pep
+  dumpDeployment test dummy-service-pep
+  dumpDeployment um pdp-engine nojson
+}
 
-# # data-access-pep
-# echo -n "Dumping policy for data-access-pep to $ORIG_DIR/data-access-pep.json..."
-# kubectl -n rm exec -it svc/data-access-pep -c data-access-pep -- management_tools list --all | jq > "$ORIG_DIR"/data-access-pep.json
-# echo " done"
+dumpDeployment() {
+  namespace="${1}"
+  deployment="${2}"
+  print_json="$( [ "${3}" = "nojson" ] && echo "" || echo "| jq" )"
+  dumpfile="${ORIG_DIR}/${deployment}.json"
 
-# ades-pep
-echo -n "Dumping policy for ades-pep to $ORIG_DIR/ades-pep.json..."
-kubectl -n proc exec -it svc/ades-pep -c ades-pep -- management_tools list --all > "$ORIG_DIR"/ades-pep.json
-echo " done"
+  echo -n "Dumping policy for ${namespace}/${deployment} to ${dumpfile}"
+  cmd="kubectl -n "${namespace}" exec -it deploy/"${deployment}" -c "${deployment}" -- management_tools list --all ${print_json}"
+  eval "${cmd}" > "${dumpfile}"
+  echo " done"
+}
 
-# workspace-api-pep
-echo -n "Dumping policy for workspace-api-pep to $ORIG_DIR/workspace-api-pep.json..."
-kubectl -n rm exec -it svc/workspace-api-pep -c workspace-api-pep -- management_tools list --all > "$ORIG_DIR"/workspace-api-pep.json
-echo " done"
-
-# dummy-service-pep
-echo -n "Dumping policy for dummy-service-pep to $ORIG_DIR/dummy-service-pep.json..."
-kubectl -n test exec -it svc/dummy-service-pep -c dummy-service-pep -- management_tools list --all > "$ORIG_DIR"/dummy-service-pep.json
-echo " done"
-
-# pdp
-echo -n "Dumping policy for pdp to $ORIG_DIR/pdp.json..."
-kubectl -n um exec -it svc/pdp-engine -c pdp-engine -- management_tools list --all > "$ORIG_DIR"/pdp.json
-echo " done"
+main "$@"
