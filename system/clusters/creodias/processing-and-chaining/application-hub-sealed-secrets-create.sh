18c5e51035a0a8232dc547553a43d982715f4b14
bbrauzzi <blasco.brauzzi@terradue.com>
2023-02-27 17:12:25 +0100

    application hub with sealed secrets

---
+++ system/clusters/creodias/processing-and-chaining/application-hub-sealed-secrets-create.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ORIG_DIR="$(pwd)"
+cd "$(dirname "$0")"
+BIN_DIR="$(pwd)"
+
+onExit() {
+  cd "${ORIG_DIR}"
+}
+
+trap onExit EXIT
+
+SECRET_NAME="application-hub-secrets"
+NAMESPACE="proc"
+
+# JUPYTERHUB_CRYPT_KEY=$(openssl rand -hex 32)
+# OAUTH_CLIENT_ID="${1:-set-client-id-here}"
+# OAUTH_CLIENT_SECRET="${2:-set-client-secret-here}"
+
+
+
+secretYaml() {
+  kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
+    --from-literal="JUPYTERHUB_CRYPT_KEY=${JUPYTERHUB_CRYPT_KEY}" \
+    --from-literal="OAUTH_CLIENT_ID=${OAUTH_CLIENT_ID}" \
+    --from-literal="OAUTH_CLIENT_SECRET=${OAUTH_CLIENT_SECRET}" \
+    --dry-run=client -o yaml
+}
+
+# Create Secret and then pipe to kubeseal to create the SealedSecret
+secretYaml | kubeseal -o yaml --controller-name eoepca-sealed-secrets --controller-namespace infra > application-hub-sealed-secrets.yaml
