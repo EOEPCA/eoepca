5c2e01f27413fc781fd6da9ffa830c8d891674e6
Daniel Pimenta <daniel.pimenta@deimos.com.pt>
2023-10-18 10:57:50 +0100

    Add django portal with keycloak auth

--- system/clusters/creodias/system/demo/ss-django-secrets-create.sh
+++ system/clusters/creodias/system/demo/ss-django-secrets-create.sh
@@ -16,12 +16,16 @@ NAMESPACE="demo"
 client_id="${1:-set-client-id-here}"
 client_secret="${2:-set-client-secret-here}"
 django_secret="${3:-set-django-secret-here}"
+keycloak_client_id="${4:-set-keycloak-client-id-here}"
+keycloak_client_secret="${5:-set-keycloak-client-secret-here}"
 
 secretYaml() {
   kubectl -n "${NAMESPACE}" create secret generic "${SECRET_NAME}" \
     --from-literal=OIDC_RP_CLIENT_ID="${client_id}" \
     --from-literal=OIDC_RP_CLIENT_SECRET="${client_secret}" \
     --from-literal=DJANGO_SECRET="${django_secret}" \
+    --from-literal=KEYCLOAK_OIDC_RP_CLIENT_ID="${keycloak_client_id}" \
+    --from-literal=KEYCLOAK_OIDC_RP_CLIENT_SECRET="${keycloak_client_secret}" \
     --dry-run=client -o yaml
 }
 
